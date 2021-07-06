4bdb4eed22a41b693d4d1a367bfd269f93a7dcba
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-06 14:24:48 -0500

    adds base homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <>
+            <h1>Hello, welcome to my blog.</h1>
+            <p>
+                Veniam excepteur anim dolore consectetur. Anim enim aute non
+                esse pariatur aliquip proident. Mollit cillum ea occaecat ut
+                aute minim velit. Ullamco fugiat cillum ipsum nisi consequat
+                fugiat mollit tempor. Dolor aliqua ipsum aliquip aliquip est
+                eiusmod. Ut exercitation veniam Lorem amet magna ex qui. Amet et
+                dolore sint est.
+            </p>
+            <p>
+                Nisi cillum ullamco minim magna nulla reprehenderit irure. Qui
+                aliquip consectetur ea ut velit et commodo sint sunt ipsum
+                eiusmod. Magna officia aliquip aliqua proident nulla proident
+                veniam minim duis cillum eiusmod excepteur reprehenderit. Nulla
+                labore reprehenderit labore officia aliqua nostrud et laboris ad
+                sit laborum ullamco nostrud in.
+            </p>
+        </>
+    );
+};
+
+export default HomePage;
