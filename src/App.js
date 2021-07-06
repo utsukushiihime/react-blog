d42db462e667493b1cf9c0d297a61c5523c61b7b
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-06 14:50:02 -0500

    adds react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
     return (
-        <div className='App'>
-            <HomePage />
-        </div>
+        <Router>
+            <main>
+                <Route exact path='/' component={HomePage} />
+            </main>
+        </Router>
     );
 }
 
