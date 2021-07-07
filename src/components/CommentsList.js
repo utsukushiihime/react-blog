7786d59a748de59a794038f6b3b1d29891a2aae8
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-06 22:03:43 -0500

    adds comments to article page

---
+++ src/components/CommentsList.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CommentsList = ({ comments }) => {
+    return (
+        <>
+            <h3>Comments:</h3>
+            {comments.map((comment, key) => (
+                <div key={key}>
+                    <h4>{comment.username}</h4>
+                    <p>{comment.text}</p>
+                </div>
+            ))}
+            <hr />
+        </>
+    );
+};
+
+export default CommentsList;
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ArticlesList from "../components/ArticlesList";
+import CommentsList from "../components/CommentsList";
 import NotFound from "./NotFound";
 import articleContent from "./article-content";
 
@@ -7,19 +8,36 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find((article) => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0,
+        comments: [],
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        };
+        fetchData();
+    }, [name]);
+
     if (!article) return <NotFound />;
 
     const otherArticles = articleContent.filter(
         (article) => article.name !== name
     );
-
     return (
         <>
             <div className="container mt-4">
                 <h1>{article.title}</h1>
+                <p>This post has been upvoted {articleInfo.upvotes} times.</p>
                 {article.content.map((paragraph, key) => (
                     <p key={key}>{paragraph}</p>
                 ))}
+
+                <CommentsList comments={articleInfo.comments} />
                 <h3>Other Articles</h3>
                 <ArticlesList articles={otherArticles} />
             </div>
